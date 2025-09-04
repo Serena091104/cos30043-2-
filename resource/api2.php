e0c653bc4690b62e2d3120e6909bd855ecd3f943
Serena091104 <147823048+Serena091104@users.noreply.github.com>
2025-09-05 03:07:25 +1000

    Update api2.php

--- resource/api2.php
+++ resource/api2.php
@@ -3,10 +3,15 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-header('Content-Type: application/json');
 header("Access-Control-Allow-Origin: https://serena091104.github.io");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
+header('Content-Type: application/json');
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(204); // No content
+    exit();
+}
 // Connect to the database
 //$conn = new mysqli("feenix-mariadb.swin.edu.au", "s104480538", "091104", "s104480538_db");
 $conn = new mysqli("sql100.infinityfree.com", "if0_39837779", "0919BxvuQ4NiRmfL321332", "if0_39837779_s104480538_db");
