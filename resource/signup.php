91d21f75adbaa8455f005643ddf571b477af8f6f
Serena091104 <147823048+Serena091104@users.noreply.github.com>
2025-09-05 02:48:59 +1000

    Update signup.php

--- resource/signup.php
+++ resource/signup.php
@@ -14,7 +14,7 @@ header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: https://serena091104.github.io');
 header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type');
-header('Access-Control-Allow-Origin: *');
+
 
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
